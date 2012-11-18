8833c17c7d7161e5679661a727a99342c5019def
Andrew Fray <afray@sega.co.uk>
2012-11-18 22:13:14 +0000

    added trem instrument

--- src/overcounters/core.clj
+++ src/overcounters/core.clj
@@ -2,18 +2,23 @@
 
 (use 'overtone.live)
 
+(defn note->hz [music-note] (midi->hz(note music-note))) 
+
 (defn n 
   ([note-name duration] {:pitch (note->hz note-name) :duration duration})
   ([note-name] (n note-name 1)))
 
 (def closenotes [(n :G4) (n :A4) (n :F4) (n :F3) (n :C4 2)])
 
-(defn note->hz [music-note] (midi->hz(note music-note))) 
-
 (definst saw-wave-inst [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
   (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
      (saw freq)
      vol))
+
+(definst trem [freq 440 depth 10 rate 6 length 3]
+    (* 0.3
+       (line:kr 0 1 length FREE)
+       (saw (+ freq (* depth (sin-osc:kr rate))))))
 
 (defn saw-wave [note-name] (saw-wave-inst (note->hz note-name)))
 
